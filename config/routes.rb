34ec7140fa74a0c257bbf384045815e3f633a8de
Martin Nikolov <martin.nikolov@abv.bg>
2012-04-25 14:06:03 +0300

    Finish Demo App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Demoapp::Application.routes.draw do
+﻿Demoapp::Application.routes.draw do
+  resources :microposts
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
